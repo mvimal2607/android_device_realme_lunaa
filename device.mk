1f0650ecdf610ab256fe28188070f33c3dfea7a2
gotenksIN <forumomkar@gmail.com>
2022-06-08 05:55:41 +0000

    lunaa: Set default hotspot ssid
    Change-Id: I7818971a6f5ac6e3307df444e4f99e7c377f512e

--- device.mk
+++ device.mk
@@ -59,7 +59,8 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
-    OPlusSystemUIResTarget
+    OPlusSystemUIResTarget \
+    OPlusWifiResTarget
 
 # Sensors
 PRODUCT_PACKAGES += \
