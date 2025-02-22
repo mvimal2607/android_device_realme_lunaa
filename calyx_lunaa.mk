38fc1b957a04d80f019babdfc857601a98dd3bc9
mvimal2607 <mvimal2607@gmail.com>
2025-02-22 12:09:25 +0000

    lunaa: Bringup CalyxOS
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- calyx_lunaa.mk
+++ calyx_lunaa.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/lunaa/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
 
-PRODUCT_NAME := lineage_lunaa
+PRODUCT_NAME := calyx_lunaa
 PRODUCT_DEVICE := lunaa
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := Realme
