239a885553ecfe756d793802c78c60b438933ec4
Vimal <mvimal2607@proton.me>
2025-12-26 04:19:59 +0100

    lunaa: Initial bringup for @yaap
    Change-Id: Id91c3db78cdfb6f87cc9655708d15b3486405bc9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lunaa.mk
+    $(LOCAL_DIR)/yaap_lunaa.mk
--- device.mk
+++ device.mk
@@ -53,7 +53,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
